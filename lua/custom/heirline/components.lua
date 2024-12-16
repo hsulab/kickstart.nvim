10f4d13a66d4563a4c74b083b169488e9b44fe51
jyxu <ahcigar@foxmail.com>
2024-12-15 22:34:24 -0500

    add heirline winbar with aerial

--- lua/custom/heirline/components.lua
+++ lua/custom/heirline/components.lua
@@ -1,3 +1,4 @@
+local aerial = require 'aerial'
 local palette = require('catppuccin.palettes').get_palette 'mocha'
 local utils = require 'heirline.utils'
 local conditions = require 'heirline.conditions'
@@ -382,6 +383,43 @@ M.FilePathBlock = {
   M.FileIcon,
   M.FilePath,
   M.FileFlags,
+}
+
+-- WinBar components
+local function format_symbol_status(symbols, depth, separator, icons_enabled)
+  local parts = {}
+  depth = depth or #symbols
+
+  if depth > 0 then
+    symbols = { unpack(symbols, 1, depth) }
+  else
+    symbols = { unpack(symbols, #symbols + 1 + depth) }
+  end
+
+  for _, symbol in ipairs(symbols) do
+    if icons_enabled then
+      table.insert(parts, string.format('%s%s', symbol.icon, symbol.name))
+    else
+      table.insert(parts, symbol.name)
+    end
+  end
+
+  return table.concat(parts, separator)
+end
+
+-- The API to output the symbols structure
+local function output_symbols_structure(depth, separator, icons_enabled)
+  local symbols = aerial.get_location(true)
+  local symbols_structure = format_symbol_status(symbols, depth, separator, icons_enabled)
+  return symbols_structure
+end
+
+M.AerialSymbols = {
+  init = function(self) end,
+  hl = { bg = palette.base, fg = palette.mauve, bold = true },
+  provider = function(self)
+    return output_symbols_structure(3, '>', true)
+  end,
 }
 
 -- Tab components
--- lua/custom/plugins/heirline.lua
+++ lua/custom/plugins/heirline.lua
@@ -3,9 +3,21 @@ return {
   events = 'VeryLazy',
   config = function()
     vim.opt.cmdheight = 0
+    local conditions = require 'heirline.conditions'
     require('heirline').setup {
       statusline = require 'custom.heirline.statusline',
       tabline = require 'custom.heirline.tabline',
+      winbar = require 'custom.heirline.winbar',
+      opts = {
+        -- if the callback returns true, the winbar will be disabled for that window
+        -- the args parameter corresponds to the table argument passed to autocommand callbacks. :h nvim_lua_create_autocmd()
+        disable_winbar_cb = function(args)
+          return conditions.buffer_matches({
+            buftype = { 'nofile', 'prompt', 'help', 'quickfix' },
+            filetype = { '^git.*', 'fugitive', 'Trouble', 'dashboard' },
+          }, args.buf)
+        end,
+      },
     }
   end,
 }
