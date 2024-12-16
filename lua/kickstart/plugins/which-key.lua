a519fd8989e00715736148c62c3a55171559f938
jyxu <ahcigar@foxmail.com>
2024-12-15 23:45:17 -0500

    add vimtex

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -68,6 +68,7 @@ return {
         { '<leader>w', group = '[W]orkspace' },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        { '<localleader>l', group = '[L]atex' },
       },
     },
   },
