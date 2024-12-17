b96f4099286c6f2fbc2b144a72c07f3c669dcd29
jyxu <ahcigar@foxmail.com>
2024-12-16 23:38:06 -0500

    move plugins to custom

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -37,13 +37,7 @@ require('lazy').setup({
 
   require 'plugins/catppuccin',
 
-  require 'plugins/todo-comments',
-
   require 'plugins/treesitter',
-
-  require 'plugins/comment',
-
-  require 'plugins/lazygit',
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
