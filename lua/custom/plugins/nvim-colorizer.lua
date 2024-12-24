d3378fe988ff9dac789b446d9a04d17517b75abe
jyxu <ahcigar@foxmail.com>
2024-12-23 22:34:08 -0500

    update colorizer

--- lua/custom/plugins/nvim-colorizer.lua
+++ lua/custom/plugins/nvim-colorizer.lua
@@ -2,6 +2,6 @@ return {
   'NvChad/nvim-colorizer.lua',
   event = 'BufReadPre',
   config = function()
-    require('colorizer').setup()
+    require('colorizer').setup { user_default_options = { names = false } }
   end,
 }
