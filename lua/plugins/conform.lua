73f18a8a3c8317511d2744810570dce53a00cf07
jyxu <ahcigar@foxmail.com>
2024-12-30 09:50:44 -0500

    conform: update stylua args

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -40,6 +40,14 @@ return {
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
       },
+      formatters = {
+        stylua = {
+          args = {
+            '--indent-type',
+            'Spaces',
+          },
+        },
+      },
     },
   },
 }
