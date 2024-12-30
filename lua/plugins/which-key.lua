dd48732539c10718be0528d2e467a8f317195765
jyxu <ahcigar@foxmail.com>
2024-12-30 14:39:16 -0500

    oil: add oil and update keys

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -15,8 +15,8 @@
 
 return {
   -- Useful plugin to show you pending keybinds.
-  'folke/which-key.nvim',
-  event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+  "folke/which-key.nvim",
+  event = "VimEnter", -- Sets the loading event to 'VimEnter'
   opts = {
     icons = {
       -- set icon mappings to true if you have a Nerd Font
@@ -24,64 +24,65 @@ return {
       -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
       -- default which-key.nvim defined Nerd Font icons, otherwise define a string table
       keys = vim.g.have_nerd_font and {} or {
-        Up = '<Up> ',
-        Down = '<Down> ',
-        Left = '<Left> ',
-        Right = '<Right> ',
-        C = '<C-…> ',
-        M = '<M-…> ',
-        D = '<D-…> ',
-        S = '<S-…> ',
-        CR = '<CR> ',
-        Esc = '<Esc> ',
-        ScrollWheelDown = '<ScrollWheelDown> ',
-        ScrollWheelUp = '<ScrollWheelUp> ',
-        NL = '<NL> ',
-        BS = '<BS> ',
-        Space = '<Space> ',
-        Tab = '<Tab> ',
-        F1 = '<F1>',
-        F2 = '<F2>',
-        F3 = '<F3>',
-        F4 = '<F4>',
-        F5 = '<F5>',
-        F6 = '<F6>',
-        F7 = '<F7>',
-        F8 = '<F8>',
-        F9 = '<F9>',
-        F10 = '<F10>',
-        F11 = '<F11>',
-        F12 = '<F12>',
+        Up = "<Up> ",
+        Down = "<Down> ",
+        Left = "<Left> ",
+        Right = "<Right> ",
+        C = "<C-…> ",
+        M = "<M-…> ",
+        D = "<D-…> ",
+        S = "<S-…> ",
+        CR = "<CR> ",
+        Esc = "<Esc> ",
+        ScrollWheelDown = "<ScrollWheelDown> ",
+        ScrollWheelUp = "<ScrollWheelUp> ",
+        NL = "<NL> ",
+        BS = "<BS> ",
+        Space = "<Space> ",
+        Tab = "<Tab> ",
+        F1 = "<F1>",
+        F2 = "<F2>",
+        F3 = "<F3>",
+        F4 = "<F4>",
+        F5 = "<F5>",
+        F6 = "<F6>",
+        F7 = "<F7>",
+        F8 = "<F8>",
+        F9 = "<F9>",
+        F10 = "<F10>",
+        F11 = "<F11>",
+        F12 = "<F12>",
       },
     },
   },
   config = function()
-    require('which-key').setup {
+    require("which-key").setup({
       ---@param ctx { mode: string, operator: string }
       defer = function(ctx)
-        if vim.list_contains({ 'd', 'y' }, ctx.operator) then
+        if vim.list_contains({ "d", "y" }, ctx.operator) then
           return true
         end
-        return vim.list_contains({ 'v', '<C-V>', 'V' }, ctx.mode)
+        return vim.list_contains({ "v", "<C-V>", "V" }, ctx.mode)
       end,
-      preset = 'helix',
-    }
+      preset = "helix",
+    })
     -- Document existing key chains
-    require('which-key').add {
-      { '<leader>c', group = 'Close Buffer', hidden = true },
-      { '<leader>e', group = 'Explorer', hidden = true },
-      { '<leader>f', group = 'Find', mode = 'n' },
-      { '<leader>g', group = 'Git', mode = { 'n', 'v' } },
-      { '<leader>l', group = 'Lsp', mode = 'n', icon = '󰿘 ' },
-      { '<leader>o', group = 'Overseer', mode = 'n', icon = '󰑮 ' },
-      { '<leader>p', group = 'Package', mode = 'n', icon = ' ' },
-      { '<leader>s', group = 'Session', mode = 'n' },
-      { '<leader>t', group = 'Terminal', mode = 'n' },
-      { '<leader>z', group = 'Zen Mode', hidden = true },
-      { '<leader>Z', group = 'Zen Mode', hidden = true },
-      { '<leader>/', group = 'Comment', hidden = true },
-      { '<localleader>l', group = 'Latex', icon = ' ' },
-    }
+    require("which-key").add({
+      { "<leader>c", group = "Close Buffer", hidden = true },
+      { "<leader>e", group = "Explorer", hidden = true },
+      { "<leader>f", group = "Find", mode = "n" },
+      { "<leader>g", group = "Git", mode = { "n", "v" } },
+      { "<leader>l", group = "Lsp", mode = "n", icon = "󰿘 " },
+      { "<leader>o", group = "Oil", hidden = true },
+      { "<leader>p", group = "Package", mode = "n", icon = " " },
+      { "<leader>r", group = "Overseer", mode = "n", icon = "󰑮 " },
+      { "<leader>s", group = "Session", mode = "n" },
+      { "<leader>t", group = "Terminal", mode = "n" },
+      { "<leader>z", group = "Zen Mode", hidden = true },
+      { "<leader>Z", group = "Zen Mode", hidden = true },
+      { "<leader>/", group = "Comment", hidden = true },
+      { "<localleader>l", group = "Latex", icon = " " },
+    })
   end,
 }
 -- vim: ts=2 sts=2 sw=2 et
